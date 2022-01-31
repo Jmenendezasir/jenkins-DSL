1d40f7765e513c2781467ce3352371694df6bfa5
jmenendezasir <jmenendezasir@gmail.com>
2022-01-31 10:55:19 +0100

    31-01-22

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -21,6 +21,6 @@ job('App-Nodejs-Docker-DSL') {
             createFingerprints(false)
             skipDecorate()
         }
-    shell("bash run.sh")
+    shell("bash /jenkinsNode/DSL/run.sh")
     }
 }
